e0810e80d0a43ee46ad22de2759aa9cd32802ef3
김진성 <kjs4082@naver.com>
2022-07-28 21:59:40 +0900

    LinkedList 를 이용하여 끝에서 K번째 값 찾기

---
+++ src/study/chap01/FindLastKNode.java
@@ -0,0 +1,64 @@
+package study.chap01;
+
+import java.util.Iterator;
+import java.util.LinkedList;
+
+/**
+ * 끝에서 K번째 노드를 찾는 알고리즘
+ *
+ * 방법 1) 전체 노드의 갯수 = N 일때 , N-K+1 의 위치 구하기
+ * 방법 2) 재귀함수사용 ,, 모르겠음
+ * 방법 3) 포인터 전략 사용
+ */
+public class FindLastKNode {
+
+    public static void main(String[] args) {
+        // 리스트 선언
+        LinkedList<Integer> linkedList = new LinkedList<>();
+
+        // 리스트 값 삽입
+        linkedList.add(2);
+        linkedList.add(5);
+        linkedList.add(9);
+        linkedList.add(6);
+        linkedList.add(1);
+        linkedList.add(10);
+        linkedList.add(3);
+
+        retrieveList(linkedList);
+        findLastNode1(linkedList, 2);
+        findLastNode3(linkedList, 2);
+    }
+    public static void retrieveList(LinkedList linkedList) {
+        Iterator<Integer> iter = linkedList.iterator();
+
+        while(iter.hasNext()) {
+            System.out.print(iter.next());
+            if(iter.hasNext()) {
+                System.out.print(" -> ");
+            }
+        }
+        System.out.println();
+    }
+
+    public static void findLastNode1(LinkedList linkedList, int k) {
+        // 리스트의 전체 노드 갯수 구하기 = N
+        // N-K+1 위치의 값 구하기 , 인덱스는 0 부터 시작이라는 점 주의 !!
+        System.out.println(linkedList.get(linkedList.size()-k));
+
+    }
+
+    public static void findLastNode3(LinkedList linkedList, int k) {
+        // 포인터 두개를 이용하여 첫번째 포인터의 시작 위치는 k , 두번째 포인터는 0에서 시작
+        // 하나씩 이동하면서 첫번째 포인터가 마지막까지 도착했을때 두번째 포인터의 위치를 리턴
+        int p1 = k;
+        int p2 = 0;
+
+        while(p1 != linkedList.size()) {
+            p2++;
+            p1++;
+        }
+
+        System.out.println(linkedList.get(p2));
+    }
+}
---
+++ src/study/chap01/Node.java
@@ -0,0 +1,40 @@
+package study.chap01;
+
+/**
+ * 단방향 LinkedList 구현하기
+ */
+public class Node {
+
+    int data;
+    Node next = null;
+
+    public Node(int d) {
+        this.data = d;
+    }
+
+    // this 는 참조변수로 인스턴스 자신을 가리킨다
+    // 삽입
+    public void append(int d) {
+        Node end = new Node(d);
+        Node n = this;
+
+        while(this.next != null) {
+            n = n.next;
+        }
+        n.next = end;
+    }
+
+    // 삭제
+    public void delete(int d) {
+        Node n = this;
+
+        while(n.next != null) {
+            if(n.next.data == d) {
+                // 다음 노드로 연결
+                n.next = n.next.next;
+            } else {
+                n = n.next;
+            }
+        }
+    }
+}
