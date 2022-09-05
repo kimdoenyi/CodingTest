bf2493d7a2ed4c40e89b48f42e2a8cfc7469bcd7
김진성 <kjs4082@naver.com>
2022-09-05 20:20:57 +0900

    BruteForce 1

---
+++ src/study/BruteForce/Main1476.java
@@ -0,0 +1,44 @@
+package study.BruteForce;
+
+import java.util.Scanner;
+
+public class Main1476 {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int e = sc.nextInt();
+        int s = sc.nextInt();
+        int m = sc.nextInt();
+
+        int year = 0;
+        int E = 0;
+        int S = 0;
+        int M = 0;
+
+        while(true) {
+            E++;
+            S++;
+            M++;
+            year++;
+
+            if(E == 16) {
+                E = 1;
+            }
+
+            if(S == 29) {
+                S = 1;
+            }
+
+            if(M == 20) {
+                M = 1;
+            }
+
+            if(E == e && S == s && M == m) {
+                break;
+            }
+        }
+
+        System.out.println(year);
+    }
+}
